6821ac26bd505072e9f4ce04ae24b6f902b8d230
FadirTorcer <eric.lyonford@gmail.com>
2022-10-27 12:56:08 -0500

    One out of three Exercise added information

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,9 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 /**
@@ -10,12 +14,33 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank(message="Name is required")
+    @Size(min=3, max=50, message="Name must be between 3 and 50 characters.")
     private String name;
+
+    @Size(max=500, message="Description too long.")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message="Email is required.")
+    @Email(message="Invalid email. Try again.")
+    private String contactEmail;
+
+    private EventType type;
+
+    @NotBlank(message="You must share where the event will be held.")
+    @NotNull(message="There must be a location.")
+    private String location;
+
+    public Event(String name, String description, String contactEmail, EventType type, String location) {
+        this();
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.type = type;
+        this.location = location;
+    }
+
+    public Event() {
         this.id = nextId;
         nextId++;
     }
@@ -34,6 +59,30 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     public int getId() {
